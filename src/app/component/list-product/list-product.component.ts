1e7c9cfffc5cefba32fc23d7779070ac8ec39d17
hamin <hamin@DESKTOP-0M3OSCA>
2021-01-02 11:45:20 +0700

    getDataDiff

--- src/app/component/list-product/list-product.component.ts
+++ src/app/component/list-product/list-product.component.ts
@@ -21,21 +21,28 @@ export class ListProductComponent implements OnInit {
     this.subscriptionTimer = interval(1000).pipe(filter(() => Boolean(this.products))).subscribe(() => {
       this.products.forEach(product => {
         const currentDate = new Date();
-        const startTime = new Date(product.StartDateTime);
-        const endTime = new Date(product.EndDateTime);
-
+        const startTime   = new Date(product.StartDateTime);
+        const endTime     = new Date(product.EndDateTime);
         if (currentDate.getTime() < startTime.getTime()) {
           product.Status = 0;
         } else if (currentDate.getTime() > endTime.getTime()) {
           product.Status = 2;
         } else {
           product.Status = 1;
-          // product['timer'] = Tool.getDataDiff(currentDate, endTime);
+          product.Timer = this.getDataDiff(currentDate, endTime);
+          console.log(product.Timer);
         }
       });
     });
   }
-
+   getDataDiff(startDate, endDate) {
+    const diff = endDate.getTime() - startDate.getTime();
+    const days = Math.floor(diff / (60 * 60 * 24 * 1000));
+    const hours = Math.floor(diff / (60 * 60 * 1000)) - (days * 24);
+    const minutes = Math.floor(diff / (60 * 1000)) - ((days * 24 * 60) + (hours * 60));
+    const seconds = Math.floor(diff / 1000) - ((days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60));
+    return { day: days, hour: hours, minute: minutes, second: seconds };
+  }
   ngOnDestroy() {
     this.subscriptionTimer.unsubscribe();
   }
