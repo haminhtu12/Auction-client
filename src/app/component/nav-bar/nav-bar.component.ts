198a198a38798acd951fa40f13ca892d1b52f3dc
hamin <hamin@DESKTOP-0M3OSCA>
2021-01-05 05:51:16 +0700

    html navbar

--- src/app/component/nav-bar/nav-bar.component.ts
+++ src/app/component/nav-bar/nav-bar.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  isCollapse = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+  tooggleMenu() {
+    this.isCollapse = !this.isCollapse;
   }
 
 }
