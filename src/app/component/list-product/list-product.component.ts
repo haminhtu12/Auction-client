3d0e2533535c047acefec3a3989e8343397f1a50
hamin <hamin@DESKTOP-0M3OSCA>
2021-01-11 02:37:36 +0700

    display current price

--- src/app/component/list-product/list-product.component.ts
+++ src/app/component/list-product/list-product.component.ts
@@ -4,8 +4,8 @@ import { Subscription } from 'rxjs';
 import {interval} from 'rxjs';
 import {filter} from 'rxjs/operators';
 import {Tool} from '../../service/tool/tool';
-import {log} from "util";
-
+import {AuctionService} from '../../service/auction/auction.service';
+import _ from 'lodash';
 @Component({
   selector: 'app-list-product',
   templateUrl: './list-product.component.html',
@@ -15,10 +15,14 @@ export class ListProductComponent implements OnInit {
   @Input()
   products: any[];
   subscriptionTimer: Subscription;
-
-  constructor() { }
-
+  listAuction: any;
+  test: {
+    Created: any,
+  };
+  constructor(private auctionService: AuctionService) { }
   ngOnInit() {
+    // tslint:disable-next-line:prefer-const one-variable-per-declaration
+    this.getListAuction();
     this.subscriptionTimer = interval(1000).pipe(filter(() => Boolean(this.products))).subscribe(() => {
       this.products.forEach(product => {
         const currentDate = new Date();
@@ -31,11 +35,26 @@ export class ListProductComponent implements OnInit {
         } else {
           product.Status = 1;
           product.Timer = Tool.getDataDiff(currentDate, endTime);
+          product.currentAuction = this.listAuction.filter(v => v.ProAutionID == product.ID);
+          product.currentBird = product.currentAuction.length;
+          // product.Created = product.currentAuction.map(a => a.Created);
+          product.currentAuction.forEach( e => e.Created =  new Date(e.Created));
+          product.max = product.currentAuction[0];
+          for (let i =0 ; i < product.currentAuction.length - 1 ; i++){
+            if (product.max.Created < product.currentAuction[i].Created) product.max =  product.currentAuction[i];
+          }
         }
       });
     });
   }
   ngOnDestroy() {
     this.subscriptionTimer.unsubscribe();
+  }
+  getListAuction(){
+    this.auctionService.findAll().subscribe(res => {
+      if (res) {
+        this.listAuction = res;
+      }
+    });
   }
 }
